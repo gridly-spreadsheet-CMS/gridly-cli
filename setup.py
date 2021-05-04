1fe2fc4ecbd637a990b409faee5650f849326c36
Lynh Dang <ld@localizedirect.com>
2021-05-04 18:44:42 +0700

    Fix pip build

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   author = 'Gridly',                   # Type in your name
   author_email = 'cn@localizedirect.com',      # Type in your E-Mail
   url = 'https://gridly.com',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gridly-spreadsheet-CMS/gridly-cli/archive/refs/tags/0.1.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/gridly-spreadsheet-CMS/gridly-cli/archive/refs/tags/0.1.7.tar.gz',    # I explain this later on
   keywords = ['GRIDLY', 'CLI', 'CMS', 'GRIDLY CLI'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'click',
